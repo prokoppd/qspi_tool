0294c44804258158eb5795a0266d2e423e7e0b46
Daniel Prokopp <danielprokopp2@gmail.com>
2025-08-14 07:18:23 +0100

    Add QSPI documentation on prototypes

--- qspi.h
+++ qspi.h
@@ -1,7 +1,27 @@
 #ifndef QSPI_H
 #define QSPI_H
 
-void QSPI_Init(void);
+
+
+
+/**
+ * @brief Initializes the QSPI (Quad SPI) interface.
+ */
+void QSPI_Init();
+
+/**
+ * @brief Checks if the QSPI interface is initialized.
+ *
+ * @return 1 if initialized, 0 otherwise.
+ */
+int QSPI_IsInitialized();
+
+/**
+ * @brief De-initializes the QSPI interface and cleans up resources.
+ */
+void QSPI_DeInit();
+
+
 
 #endif // QSPI_H
 
