f0888f06045bfc474d7826620c37d748be83ce14
Daniel Prokopp <danielprokopp2@gmail.com>
2025-08-14 07:17:38 +0100

    Added QSPI Init Check

--- main.c
+++ main.c
@@ -3,12 +3,26 @@
 
 #include "slog.h"
 #include "qspi.h"
+#include <assert.h>
 
 int main()
 {
+    assert(QSPI_IsInitialized() == 0 && "QSPI should not be initialized at the start");
     slog_init("qspi_tool", SLOG_FLAGS_ALL, 0);
     slog_info("Starting QSPI tool...");
     slog_info("Initializing QSPI...");
     QSPI_Init();
+    
+    if (QSPI_IsInitialized())
+    {
+        slogi("QSPI initialized successfully");
+    }
+    else
+    {
+        slogf("QSPI initialization failed");
+        slog_destroy();
+        return EXIT_FAILURE;
+    }
+    
     return EXIT_SUCCESS;
 }
