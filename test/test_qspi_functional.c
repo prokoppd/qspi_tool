4b7442729a3d276bc4e923712ef40b568488a3fd
Daniel Prokopp <danielprokopp2@gmail.com>
2025-08-21 03:56:59 +0100

    Removed dummy tests

--- test/test_qspi_functional.c
+++ test/test_qspi_functional.c
@@ -14,15 +14,6 @@ TEST_TEAR_DOWN(QSPI_Functional)
 {
 }
 
-TEST(QSPI_Functional, pass_test)
-{
-    TEST_ASSERT_EQUAL(1, 1); // This will pass
-}
-TEST(QSPI_Functional, fail_test)
-{
-    TEST_ASSERT_EQUAL(1, 1); // This will fail
-}
-
 TEST(QSPI_Functional, QSPI_IsInitialized_before_initcall)
 {
     TEST_ASSERT_FALSE(QSPI_IsInitialized());
