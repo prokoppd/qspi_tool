16cc9a30875dfd8fc67dc5943a1974074ca15236
Daniel Prokopp <danielprokopp2@gmail.com>
2025-08-21 03:56:59 +0100

    Removed dummy tests

--- test/test_runner.c
+++ test/test_runner.c
@@ -3,7 +3,5 @@
 
 TEST_GROUP_RUNNER(QSPI_Functional)
 {
-    RUN_TEST_CASE(QSPI_Functional, pass_test);
-    RUN_TEST_CASE(QSPI_Functional, fail_test);
     RUN_TEST_CASE(QSPI_Functional, QSPI_IsInitialized_before_initcall);
 }
